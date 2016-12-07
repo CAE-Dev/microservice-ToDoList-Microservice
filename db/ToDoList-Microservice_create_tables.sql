9d1d1674216cfea0ee615c509bb2873fb1b910d0
CAEGamifiedUser <CAEGamified@gmail.com>
2016-12-07 16:58:32 +0100

    Generated microservice version 0.1

---
+++ db/ToDoList-Microservice_create_tables.sql
@@ -0,0 +1,14 @@
+--
+-- Database Schema:  gamificationCAE 
+-- Automatically generated sql script for the service ToDoList-Microservice, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table todolist.
+--
+CREATE TABLE gamificationCAE.todolist (
+  name TEXT,
+  idx INTEGER,
+  CONSTRAINT idx_PK PRIMARY KEY (idx)
+);
+
